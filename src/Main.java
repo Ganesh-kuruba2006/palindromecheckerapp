30d43d581c272551b668fc961fff29d6a1356e16
Ganesh-kuruba2006 <kurubaganesh2006@gmail.com>
2026-02-18 09:55:39 +0530

    uc1-welcome mesage

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
-public class Main git{
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("welcome to the palindrome checker mangement system");
+
+        System.out.println("version 1:0");
+
+        System.out.println("system initilaizes succesfully");
+    }
 }
